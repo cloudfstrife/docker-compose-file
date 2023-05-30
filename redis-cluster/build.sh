df0e32f4737871c182b8674d033c9b6aad91e874
Cloud Strife <cloudfstrife@sina.cn>
2023-05-30 20:32:19 +0800

    fix(redis-cluster): add multi node feature

--- redis-cluster/build.sh
+++ redis-cluster/build.sh
@@ -1,50 +1,48 @@
 #!/bin/bash
 
-NODES=8
-IP_ADDRESS=192.168.20.200
-SUBNET=172.16.0.0/24
+INS_PER_NODE=2
+NODE_LIST="10.0.0.10 10.0.0.11 10.0.0.12"
 REDIS_IMAGE=redis:7.0.11
 PASSWD=password
 
 # generate conf
-for i in $(seq 1 ${NODES});                                 \
-do                                                          \
-mkdir -p ./node-${i}/conf                                               
-cat << EOF | tee -a ./node-${i}/conf/redis.conf >/dev/null
-port `expr 6379 + ${i}`
+generate(){
+  for node in ${NODE_LIST};do                                                 \
+    mkdir -p ${node}
+    for i in $(seq 1 ${INS_PER_NODE});do                                      \
+      mkdir -p ./${node}/node-${i}/conf
+      cat << EOF | tee -a ./${node}/node-${i}/conf/redis.conf >/dev/null
+port `expr 6379 + ${i} - 1`
 masterauth ${PASSWD}
 requirepass ${PASSWD}
 bind 0.0.0.0
 cluster-enabled yes
 cluster-config-file nodes.conf
 cluster-node-timeout 5000
-cluster-announce-ip ${IP_ADDRESS}
-cluster-announce-port `expr 6379 + ${i}`
-cluster-announce-bus-port `expr 16379 + ${i}`
+cluster-announce-ip ${node}
+cluster-announce-port `expr 6379 + ${i} - 1`
+cluster-announce-bus-port `expr 16379 + ${i} - 1`
 appendonly yes
 appendfsync everysec
 no-appendfsync-on-rewrite no
 auto-aof-rewrite-percentage 100
 auto-aof-rewrite-min-size 64mb
 EOF
-done
+    done
 
-# generate docker-compose.yml
-cat << EOF | tee -a docker-compose.yml > /dev/null
+    cat << EOF | tee -a ${node}/docker-compose.yml > /dev/null
 version: "3.9"
 services:
 EOF
-
-for i in $(seq 1 ${NODES});                                 \
-do                                                          \
-cat << EOF | tee -a docker-compose.yml > /dev/null
+    for i in $(seq 1 ${INS_PER_NODE});do                                      \
+      cat << EOF | tee -a ${node}/docker-compose.yml > /dev/null
   node-${i}:
     image: ${REDIS_IMAGE}
     environment:
       TZ: Asia/Shanghai
     ports:
-      - "`expr 6379 + ${i}`:`expr 6379 + ${i}`"
-      - "`expr 16379 + ${i}`:`expr 16379 + ${i}`"
+      - "`expr 6379 + ${i} - 1`:`expr 6379 + ${i} - 1`"
+      - "`expr 16379 + ${i} - 1`:`expr 16379 + ${i} - 1`"
     volumes:
       - ./node-${i}/conf/redis.conf:/usr/local/etc/redis/redis.conf
       - ./node-${i}/data:/data
@@ -52,33 +50,32 @@ cat << EOF | tee -a docker-compose.yml > /dev/null
     command: 
       - redis-server
       - /usr/local/etc/redis/redis.conf
-
 EOF
-done
+    done
+  done
+}
 
-cat << EOF | tee -a docker-compose.yml > /dev/null
-networks:
-  proxy:
-    ipam:
-      config:
-      - subnet: ${SUBNET}
-EOF
+show(){
+  printf "create cluster\n"
+  printf "# --------------------------------------------------------------------------\n"
+  printf "docker compose exec node-1 redis-cli -a %s --cluster create %s\n" ${PASSWD} "\\"
+  for node in ${NODE_LIST};do                                                                     \
+    for i in $(seq 1 ${INS_PER_NODE});do
+    printf "%s:%d %s\n" ${node} `expr 6379 + ${i} - 1` "\\"
+    done
+  done
+  printf "%s %d\n\n" "--cluster-replicas" 1
 
-printf "start nodes\n"
-printf "# --------------------------------------------------------------------------\n"
-printf "docker compose up -d\n\n"
+  printf "connect node\n"
+  printf "# --------------------------------------------------------------------------\n"
+  for i in $(seq 1 ${INS_PER_NODE});do
+  printf "docker compose exec node-%d redis-cli -a %s -p %d \n\n" ${i} ${PASSWD} `expr 6379 + ${i} - 1`
+  done
+}
 
-printf "create cluster\n"
-printf "# --------------------------------------------------------------------------\n"
-printf "docker compose exec node-1 redis-cli -a %s --cluster create %s\n" ${PASSWD} "\\"
-for i in $(seq 1 ${NODES});do
-printf "%s:%d %s\n" ${IP_ADDRESS} `expr 6379 + ${i}` "\\"
-done
-printf "%s %d\n\n" "--cluster-replicas" 1
-
-printf "connect node\n"
-printf "# --------------------------------------------------------------------------\n"
-for i in $(seq 1 ${NODES});do
-printf "docker compose exec node-%d redis-cli -a %s -p %d \n\n" ${i} ${PASSWD} `expr 6379 + ${i}`
-done
-
+case "$1" in
+  "show") show ;;
+  "dc") generate ;;
+  *) echo "USAGE : $0 [dc|show]"
+    exit 1 ;;
+esac
